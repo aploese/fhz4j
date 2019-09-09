b7978404a6af4a5d5ae8e5b654c5c35be7e681f2
Arne Plöse <aploese@gmx.de>
2019-09-09 15:03:00 +0200

    Modularized (JPMS)

---
+++ de.ibapl.fhz4j.parser.cul/src/main/java/module-info.java
@@ -0,0 +1,37 @@
+/*
+ * FHZ4J - Drivers for the Wireless FS20, FHT and HMS protocol https://github.com/aploese/fhz4j/
+ * Copyright (C) 2009-2019, Arne Plöse and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+module de.ibapl.fhz4j.parser.cul {
+	requires java.logging;
+	requires transitive de.ibapl.spsw.api;
+	requires org.osgi.dto;
+	requires osgi.annotation;
+	
+	exports de.ibapl.fhz4j.api;
+	exports de.ibapl.fhz4j.cul;
+	exports de.ibapl.fhz4j.parser.cul;
+	exports de.ibapl.fhz4j.protocol.em;
+	exports de.ibapl.fhz4j.protocol.evohome;
+	exports de.ibapl.fhz4j.protocol.fht;
+	exports de.ibapl.fhz4j.protocol.fs20;
+	exports de.ibapl.fhz4j.protocol.hms;
+	exports de.ibapl.fhz4j.protocol.lacrosse.tx2;
+}
