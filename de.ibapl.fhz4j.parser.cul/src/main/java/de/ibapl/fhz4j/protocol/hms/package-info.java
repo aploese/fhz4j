d8a51f340fa1b7c9d7f17b18e5e325eb400f8ceb
Arne Plöse <aploese@gmx.de>
2019-09-14 06:56:50 +0200

    updated package-info to reflect the changes

--- de.ibapl.fhz4j.parser.cul/src/main/java/de/ibapl/fhz4j/protocol/hms/package-info.java
+++ de.ibapl.fhz4j.parser.cul/src/main/java/de/ibapl/fhz4j/protocol/hms/package-info.java
@@ -19,5 +19,5 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-@org.osgi.annotation.versioning.Version("1.0.0")
+@org.osgi.annotation.versioning.Version("1.1.0")
 package de.ibapl.fhz4j.protocol.hms;
--- de.ibapl.fhz4j.parser.cul/src/main/java/de/ibapl/fhz4j/protocol/lacrosse/tx2/package-info.java
+++ de.ibapl.fhz4j.parser.cul/src/main/java/de/ibapl/fhz4j/protocol/lacrosse/tx2/package-info.java
@@ -20,4 +20,4 @@
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
 @org.osgi.annotation.versioning.Version("1.1.0")
-package de.ibapl.fhz4j.protocol.em;
+package de.ibapl.fhz4j.protocol.lacrosse.tx2;
